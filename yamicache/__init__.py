3844bd244ed72cd8cb52fea0e08e741ef8947e5e
Timothy McFadden <timothy.mcfadden@broadcom.com>
2017-09-05 15:04:14 -0600

    fix relative import bug for PY3

--- yamicache/__init__.py
+++ yamicache/__init__.py
@@ -5,4 +5,4 @@
 __author__ = """Timothy McFadden"""
 __version__ = '0.2.0'
 
-from yamicache import Cache, nocache, override_timeout   # noqa: F401
+from .yamicache import Cache, nocache, override_timeout   # noqa: F401
