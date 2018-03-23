2d213e731759aced6aeda1594f7e4509a9997c1c
Timothy McFadden <timothy.mcfadden@broadcom.com>
2018-03-23 11:34:54 -0600

    [skip ci] prep for release

--- yamicache/__init__.py
+++ yamicache/__init__.py
@@ -3,6 +3,6 @@
 """Top-level package for yamicache."""
 
 __author__ = """Timothy McFadden"""
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 from .yamicache import Cache, nocache, override_timeout   # noqa: F401
